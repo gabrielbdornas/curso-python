9fdf4182fb970da05b2122d79ca6d3f285cc64fa
Gabriel Braico Dornas <gabrielbdornas@gmail.com>
2026-05-08 14:23:39 +0000

    Add conteudo aula 04

--- aulas/04/app.py
+++ aulas/04/app.py
@@ -1,1 +1,10 @@
 
+primeiro_nome = input('Qual seu primeiro nome? ')
+ultimo_nome = input('Qual seu ultimo nome? ')
+
+nome_completo = primeiro_nome + " " + ultimo_nome
+# print mostra o valor guardado na variável nome_completo
+print(nome_completo)
+# concatenação é avaliada antes e resultado passado para argumento para
+# função print
+print("Gabriel" + " Dornas")
