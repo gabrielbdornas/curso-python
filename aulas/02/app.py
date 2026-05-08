a76e4edfa03dd063771a2df0d8b945b42f268701
Gabriel Braico Dornas <gabrielbdornas@gmail.com>
2026-05-08 13:54:39 +0000

    Add conteudo aula 02

--- aulas/02/app.py
+++ aulas/02/app.py
@@ -1,1 +1,33 @@
+# Desenho de um cachorro
+print('O----')
+print(' ||||')
 
+print()
+print()
+
+# Desenho de um robô
+print('  [::::]')
+print('  |0  0|')
+print('  | -- |')
+print('|   --   |')
+print('  | -- |')
+
+print()
+print()
+
+# Ordem da execução muda quando python:
+# - encontra uma atribuição (variável recebe um valor).
+# - variável caixinha para guardar valores, ou dar nome a coisas.
+# - encontra uma expressão (instrução que produz um valor).
+primeiro_numero = 10
+segundo_numero = 20
+print(primeiro_numero + segundo_numero)
+
+print()
+print()
+
+minha_string = '-'
+print(minha_string * 10)
+
+print('Guilherme ' * 10)
+print(2 * 10)
