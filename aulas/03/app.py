e52e6b7fa4cd16fd54f6898f2ff579d3628aa699
Gabriel Braico Dornas <gabrielbdornas@gmail.com>
2026-05-08 14:16:27 +0000

    add conteudo aula 03

--- aulas/03/app.py
+++ aulas/03/app.py
@@ -1,1 +1,16 @@
+# str: já vimos.
+# int: números inteiros (sem casas decimais).
+# float: números com casas decimais.
+# bool: valores lógicos (`True` ou `False`).
+nota = 8
+media = 7.5
+nome = "Abel"
+estudando = True # False
 
+print(nome, 'está estudando?', estudando, '(True = Sim, False = Não)')
+print(nome, 'tirou nota', nota, 'mas tem média', media)
+
+#1primeiro_numero = 10
+#segundo-numero = 20
+
+# "python -i caminho-arquivo" para testar o código criado
